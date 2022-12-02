ced36accddd56f4dd51c0cc7cc0ae84ed2788e5b
Nikita Ushanov <64222243+NikitaUshanov@users.noreply.github.com>
2022-12-02 18:23:46 +0300

    Create seminar_1.py

---
+++ seminar_1.py
@@ -0,0 +1,27 @@
+
+# task_1
+'''
+Напишите программу, которая будет открывать определенный файл CSV и выводить на печать построчно последние 10 строк. 
+Если в файле всего 10 и меньше строк, то нужно вывести только первую строчку.
+'''
+
+def check_file(path):
+    df = pd.read_csv(path)
+    if len(df) >= 10:
+        print(df.tail())
+    else:
+        print(df.head(1))
+
+check_file('prices22.csv')
+
+
+# task_2
+'''
+Создать текстовый файл File_txt.txt. В него первой строкой записать свою группу, ФИО. 
+Вторая строка – четные числа от 0 до 100. Числа должны быть разделены «;».
+'''
+with open('File_txt.txt', 'w') as text_file:
+    text_file.write('Ushanov Nikita, PUOR22-2m \n')
+
+with open('File_txt.txt', 'a') as text_file:
+    text_file.write(arr = list(range(0, 40, 2))
