463088c5d3c7d71b0b6cf85f326a4e9ec8cbb492
Nikita Ushanov <64222243+NikitaUshanov@users.noreply.github.com>
2022-12-03 08:24:28 +0300

    Update seminar_1.py

--- seminar_1.py
+++ seminar_1.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import csv
+import datetime
+import time
 
 
 # task_1
@@ -35,4 +37,42 @@ with open('File_txt.txt', 'a') as text_file:
 Напишите функцию read_last(lines, file), которая будет открывать определенный файл file и выводить на печать
 построчно последние строки в количестве lines (на всякий случай проверим, что задано положительное целое число).
 '''
+def read_last(lines, file):
+    if lines > 0:
+        with open(file, encoding='utf-8') as text:
+            file_lines = text.readlines()[-lines:]
+        for line in file_lines:
+            print(line.strip())
+    else:
+          print('Параметр "lines" должен быть больше 0!')
+            
 
+# task_4
+'''
+Требуется реализовать функцию longest_words(file), которая выводит слово, имеющее максимальную длину (или список слов, если таковых несколько).
+'''
+def longest_words(file):
+    with open(file, encoding='utf-8') as text:
+        words = text.read().split()
+        max_length = len(max(words, key=len))
+        sought_words = [word for word in words if len(word) == max_length]
+        if len(sought_words) == 1:
+            return sought_words[0]
+        else:
+            return sought_words
+        
+        
+# task_5
+'''
+Требуется создать csv-файл «rows_300.csv» со следующими столбцами:
+– № - номер по порядку (от 1 до 300);
+– Секунда – текущая секунда на вашем ПК;
+– Микросекунда – текущая миллисекунда на часах.
+На каждой итерации цикла искусственно приостанавливайте скрипт на 0,01 секунды.
+'''
+with open('rows_300.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['№', 'Секунда ', 'Микросекунда'])
+    for line in range(1, 301):
+        writer.writerow([line, datetime.datetime.now().second, datetime.datetime.now().microsecond])
+        time.sleep(0.01)
